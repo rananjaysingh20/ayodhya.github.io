a4ffb637963cca68f287ab182561e91059651c71
rananjaysingh20 <rananjaysingh20@gmail.com>
2024-04-12 03:20:33 +0530

    added font and created burger menu

--- src/Navbar/navbar.js
+++ src/Navbar/navbar.js
@@ -13,12 +13,19 @@ export const Navbar = () => {
       <nav className="navbar-container">
         <ul className={`menu ${isOpen ? "is-open-menu" : ""}`}>
           <li className="menu-trigger" onClick={toggleMenu}>
-            H
+            <div className={`burger-icon ${isOpen ? "hide-burger" : ""}`}>
+              <div className="burger-line"></div>
+              <div className="burger-line"></div>
+              <div className="burger-line"></div>
+            </div>
+            <div
+              className={`close-icon ${isOpen ? "show-close" : "hide-close"}`}
+            ></div>
           </li>
           {routes.map((route) => {
             const { href, title } = route;
             return (
-              <li className={`menu-items ${isOpen ? "is-open" : ""}`}>
+              <li key={title} className={`menu-items ${isOpen ? "is-open" : ""}`}>
                 <a href={href}>{title}</a>
               </li>
             );
